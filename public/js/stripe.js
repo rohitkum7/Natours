72bd5721b42765dcc311117b6add767130547030
Rohit kumar <rohitkumar1215225@gmail.com>
2023-12-17 10:07:28 +0530

    Prepared app for Deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,7 +5,8 @@ export const bookTour = async (tourId) => {
   try {
     //1) Get check-out session from the api server
     const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+      // `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+      `/api/v1/bookings/checkout-session/${tourId}`,
     );
     return session;
   } catch (err) {
